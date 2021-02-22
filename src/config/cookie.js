960726acb5a8b2bdcb0dc51816eb1edc8f2bb5b3
bourbour619 <bourbour619@gmail.com>
2021-02-22 15:43:21 +0330

    project updated v16

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -2,16 +2,15 @@ import moment from 'moment'
 
 export const setCookie = (name, value, days, path) => {
         const expires = moment().add(days,'d').utc().format()
-        const ck = btoa(`${name}=${value};expires=${expires};path=/${path}`)
-        document.cookie = ck
+        document.cookie = `${name}=${btoa(value)};expires=${expires};path=/${path}`
         
 }
 
 
 export const getCookie = (name) => {
-    const ck = atob(document.cookie)
-    const inCookie = ck.split(';')
-                    .find(ck => ck.startsWith(`${name}=`))
+    const inCookie = document.cookie
+                             .split(';')
+                             .find(ck => ck.startsWith(`${name}=`))
     if(!inCookie) return
-    return inCookie.split('=')[1]
+    return atob(inCookie).split('=')[1]
 }
