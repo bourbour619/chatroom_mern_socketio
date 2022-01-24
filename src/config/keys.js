8dca37c86234bd821897ef30db6757a17ac4b1ba
bourbour619 <70726766+bourbour619@users.noreply.github.com>
2022-01-24 22:23:39 +0330

    project updated v34

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,3 @@
-export const API_URL = 'http://192.168.100.106:5002/api/v1'
-// export const API_URL = 'http://46.4.203.170:5002/api/v1'
-export const SOCKET_URL = 'http://192.168.100.106:5002/'
-// export const SOCKET_URL = 'http://46.4.203.170:5003/'
+export const API_URL = 'http://46.4.203.170:5002/api/v1'
+export const SOCKET_URL = 'http://46.4.203.170:5002/'
 export const AUTH_KEY = 'naa_messenger_auth_data'
